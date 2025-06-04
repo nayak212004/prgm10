61b27823b8d913a1ab12e71b45b4ab62892447ff
nayak <nayakprathamesh2004gmail.com>
2025-06-04 20:15:29 +0530

    initial commit of maven project for azure

---
+++ src/main/java/nayak/prgm10/App.java
@@ -0,0 +1,13 @@
+package nayak.prgm10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
